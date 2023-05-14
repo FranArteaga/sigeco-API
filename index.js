708af7093298f8811eacd0abd46f61958794c9b0
francis <franrtg96@gmail.com>
2023-05-14 17:16:04 -0600

    connected to DB using environment variables

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const server = require ("./src/server")
+require('dotenv').config()
+const mongoose = require('mongoose')
+const server = require("./src/server")
 
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
 
-
-
-server.listen(8080, (request, response) => {
-    console.log("Working Server!")
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`)
+.then(() => {
+    console.log("Connected to DB!")
+    server.listen(8080, (request, response) => {
+        console.log("Working Server!")
+    })
+})
+.catch((err)=> {
+    console.log("Error with server...", err)
 })
